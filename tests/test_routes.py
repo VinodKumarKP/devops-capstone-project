7f779a40f6d0c7ab62adc5ffd78c8b77be3a1a13
VinodKumarKP <31899217+VinodKumarKP@users.noreply.github.com>
2022-12-27 22:04:50 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -151,7 +151,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-        
+    
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -173,12 +173,10 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def test_security_headers(self):
         """It should return security headers"""
@@ -193,7 +191,6 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
 
     def test_cors_security(self):
         """It should return a CORS header"""
